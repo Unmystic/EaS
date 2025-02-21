6908c4f7ab7567c747dee4a5738dc0b69dbee6a6
Unmystic <unmystic7@gmail.com>
2025-02-21 19:44:12 +0100

    Added form input for grid selection

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 'use strict'
 
 const gameBody = document.querySelector('body')
+
+// Create selector for number of drawing cells
+const form = document.createElement('form')
+const selector = document.createElement('div')
+form.appendChild(selector)
+gameBody.appendChild(form)
+
+const textLabel = document.createElement('label')
+textLabel.textContent = "Number of squares per side(10-100): "
+selector.appendChild(textLabel)
+
+const inputField = document.createElement('input')
+inputField.type = "number"
+inputField.min = 10
+inputField.max = 100
+inputField.step = 2
+selector.appendChild(inputField)
+
+const contSubmit = document.createElement('div')
+const btnSubmit = document.createElement('input')
+btnSubmit.type = "submit"
+contSubmit.appendChild(btnSubmit)
+form.appendChild(contSubmit)
+
 const container = document.createElement('div')
 gameBody.appendChild(container)
